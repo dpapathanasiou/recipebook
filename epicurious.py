761c42ac9e6183a8fa3b5d447b4f8d93fc449a3e
Denis Papathanasiou <denis@papathanasiou.org>
2017-02-05 20:09:08 -0500

    Added an implementation for 'epicurious.com'

---
+++ epicurious.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""
+epicurious.py
+
+This module inherits from RecipeParser, and provides an implementation
+for parsing recipes from the epicurious.com site.
+
+"""
+
+from lxml import etree
+
+from parser import RecipeParser
+
+class Epicurious(RecipeParser):
+    def getTitle(self):
+        """The title format is:
+
+        <title>[Recipe] recipe | Epicurious.com</title>
+
+        we want just 'Recipe'
+        """
+        return u' '.join(self.tree.xpath('//title')[0].text.split('|')[0].strip().split()[:-1])
+
+    def getIngredients(self):
+        """Return a list or a map of the recipe ingredients"""
+        data = []
+        for node in self.tree.xpath('//li[@itemprop="ingredients"]'):
+            data.append( ''.join(node.xpath('descendant-or-self::text()')).strip() )
+        return data
+
+    def getDirections(self):
+        """Return a list or a map of the preparation instructions"""
+        data = []
+        for node in self.tree.xpath('//li[@class="preparation-step"]'):
+            data.append( ''.join(node.xpath('descendant-or-self::text()')).strip() )
+        return data
+
+    def getTags(self):
+        """Return a list of tags for this recipe"""
+        data = []
+        for node in self.tree.xpath('//*[@itemprop="recipeCategory"]'):
+            data.append( ''.join(node.xpath('descendant-or-self::text()')).strip() )
+        return data
+
+    def getOtherRecipeLinks(self):
+        """Return a list of other recipes found in the page"""
+        data = []
+        for link in self.tree.xpath('//div[contains(@class,"recipes")]/ul[contains(@class,"content")]/*/a'):
+            if 'href' in link.keys():
+                l = 'http://www.epicurious.com'+link.get('href')
+                if l not in data:
+                    data.append(l)
+        return data
