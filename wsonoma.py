a455d942239ea904afdc3a79704959bb1a1349a0
Denis Papathanasiou <denis@papathanasiou.org>
2017-02-12 14:49:05 -0500

    Logic to grab individual recipe links from the williams-sonoma.com search categories

--- wsonoma.py
+++ wsonoma.py
@@ -9,6 +9,7 @@ for parsing recipes from the williams-sonoma.com site.
 """
 
 from lxml import etree
+from urlparse import urlsplit
 
 from parser import RecipeParser
 
@@ -35,11 +36,25 @@ class WilliamsSonoma(RecipeParser):
         for node in self.tree.xpath('//div[@class="directions"]'):
             data.append( node.xpath('descendant-or-self::text()') )
         return map(lambda x: x.strip(), data[0])
-        
+
     def getTags(self):
         """Return a list of tags for this recipe"""
         return []
 
     def getOtherRecipeLinks(self):
-        """Return a list of other recipes found in the page"""
-        return []
+        """Return a list of other recipes found in the page: while single recipe
+        pages do not have links, the various categories at
+        http://www.williams-sonoma.com/recipe/ do.
+
+        For example,
+        http://www.williams-sonoma.com/search/results.html?activeTab=recipes&words=winter_weeknight_dinners
+        has a collection of individual recipe links, and this method will find them.
+
+        """
+        data = []
+        for link in self.tree.xpath('//ul[@class="recipe-list"]/li/a'):
+            if 'href' in link.keys():
+                href = urlsplit(link.get('href'))
+                if 'cm_src=RECIPESEARCH' == href.query:
+                    data.append(href.scheme + '://' + href.netloc + href.path)
+        return data
