524cc4ed21cb191285b2e7e23c6a8c1355138be5
Denis Papathanasiou <denis@papathanasiou.org>
2017-02-12 17:29:07 -0500

    Enabled the inclusion of a 'local_settings.py' file in the main program settings

--- settings.py
+++ settings.py
@@ -12,3 +12,12 @@ UA = "recipebook/1.0 +http://github.com/dpapathanasiou/recipebook"
 
 # Define where the scraped and parsed results will be saved
 OUTPUT_FOLDER = '/tmp'
+
+# Override any of the above settings for your local environment in a
+# separate local_settings.py file which is *not* checked into source
+# control
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
