90cc6b0b8fc52f537ba2f5c6775b28538dc91b86
Denis Papathanasiou <denis@papathanasiou.org>
2017-07-04 13:52:41 -0400

    Added logic to generate a TOTP (RFC 6238) based on a secret seed

---
+++ totpGenerator.py
@@ -0,0 +1,22 @@
+"""
+totpGenerator.py
+
+Produces a Time Based One-Time Password (TOTP) given a secret seed
+string, as defined by RFC 6238 (https://tools.ietf.org/html/rfc6238)
+
+"""
+
+from oath import totp
+import hashlib
+
+def stringToHex (s):
+    """Convert the given string to a hexadecimal version of itself,
+       since the seed passed to totp() needs it in this form."""
+
+    return ''.join("{:02x}".format(ord(c)) for c in s)
+
+def create (seed, hash=hashlib.sha512):
+    """Create a new TOTP for the given secret seed and hash,
+       using the default 30 second time step period."""
+
+    return totp(stringToHex(seed), format='dec8', hash=hash)
