7d6ff286dba7577660116484eb4667e6523fba12
Denis Papathanasiou <denis@papathanasiou.org>
2020-05-09 16:37:16 -0400

    PEP8 updates

--- totpGenerator.py
+++ totpGenerator.py
@@ -11,13 +11,15 @@ string, as defined by RFC 6238 (https://tools.ietf.org/html/rfc6238)
 from oath import totp
 import hashlib
 
-def stringToHex (s):
+
+def stringToHex(s):
     """Convert the given string to a hexadecimal version of itself,
        since the seed passed to totp() needs it in this form."""
 
     return ''.join("{:02x}".format(ord(c)) for c in s)
 
-def create (seed, hash=hashlib.sha512):
+
+def create(seed, hash=hashlib.sha512):
     """Create a new TOTP for the given secret seed and hash,
        using the default 30 second time step period."""
 
